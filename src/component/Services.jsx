06de4579215499a5016d311c3910178801be9e68
Mohd Danish Malik <121788489+Malik113322@users.noreply.github.com>
2024-06-16 19:29:01 +0530

    Update Services.jsx

--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -28,13 +28,7 @@ const Services = () => {
     <div id='services'>
       <h2>Services</h2>
       <section>
-        <motion.div className="serviceBox1"
-          whileInView={animations.whileInView}
-          initial={animations.one}
-        >
-          <h3>1+</h3>
-          <p>Years Experience</p>
-        </motion.div>
+        
         <motion.div className="serviceBox2"
           whileInView={animations.whileInView}
           initial={animations.twoAndThree}
@@ -42,20 +36,7 @@ const Services = () => {
           <AiFillIeCircle />
           <span>Web Development</span>
         </motion.div>
-        <motion.div className="serviceBox3"
-          whileInView={animations.whileInView}
-          initial={animations.twoAndThree}
-        >
-          <AiFillAndroid />
-          <span>App Development</span>
-        </motion.div>
-        <motion.div className="serviceBox4"
-          whileInView={animations.whileInView}
-          initial={animations.four}
-        >
-          <AiFillWindows />
-          <span>Desktop Development</span>
-        </motion.div>
+          
       </section>
     </div>
   )
